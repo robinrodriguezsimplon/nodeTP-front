62cd938b9941c93c02a029b6b3f69f05f9dcf8bd
Robin <robin.rodriguez.simplon@gmail.com>
2019-12-06 15:54:09 +0100

    test

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'https://app-85b478cf-a9e0-4f1f-829b-71206bc2807a.cleverapps.io/',
+  baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
